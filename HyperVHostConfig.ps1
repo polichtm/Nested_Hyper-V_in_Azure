dcb9237740d73362d9cb3b7fd1046f1b08aab1f1
Marcin Policht <polichtm@hotmail.com>
2023-12-31 19:41:04 -0500

    Updated NAT configuration

--- HyperVHostConfig.ps1
+++ HyperVHostConfig.ps1
@@ -18,69 +18,67 @@ Microsoft Lab Environment - Azure Backup
 
 Configuration Main
 {
-	Param ( [string] $nodeName )
+   Param ( [string] $nodeName )
 
-	Import-DscResource -ModuleName 'PSDesiredStateConfiguration', 'xHyper-V'
+   Import-DscResource -ModuleName 'PSDesiredStateConfiguration', 'xHyper-V'
 
-	node $nodeName
-  	{
-		# Ensures a VM with default settings
-        xVMSwitch InternalSwitch
-        {
-            Ensure         = 'Present'
-            Name           = 'NatSwitch'
-            Type           = 'Internal'
-        }
-		
-		Script ConfigureHyperV
-    	{
-			GetScript = 
-			{
-				@{Result = "ConfigureHyperV"}
-			}	
-		
-			TestScript = 
-			{
-           		return $false
-        	}	
-		
-			SetScript =
-			{
-				$zipDownload = "https://labnestedhvartifacts.blob.core.windows.net/azurelab/HyperVLabVMs.zip"
-				$downloadedFile = "D:\HyperVLabVMs.zip"
-				$vmFolder = "C:\VM"
-				Resize-Partition -DiskNumber 0 -PartitionNumber 2 -Size (400GB)
-				Invoke-WebRequest $zipDownload -OutFile $downloadedFile
-				Add-Type -assembly "system.io.compression.filesystem"
-				[io.compression.zipfile]::ExtractToDirectory($downloadedFile, $vmFolder)
-				$NatSwitch = Get-NetAdapter -Name "vEthernet (NatSwitch)"
-				New-NetIPAddress -IPAddress 192.168.0.1 -PrefixLength 24 -InterfaceAlias $NatSwitch.Name
-				# New-NetNat -Name NestedVMNATnetwork -InternalIPInterfaceAddressPrefix 192.168.0.0/24 -Verbose
-				New-VM -Name DC01 `
-					-MemoryStartupBytes 2GB `
-					-BootDevice VHD `
-					-VHDPath 'C:\VM\AD01.vhdx' `
-		                        -Path 'C:\VM' `
-					-Generation 1 `
-				        -Switch "NATSwitch"
-				Start-VM -Name DC01
-				New-VM -Name FS01 `
-					-MemoryStartupBytes 2GB `
-					-BootDevice VHD `
-					-VHDPath 'C:\VM\FS01.vhdx' `
-					-Path 'C:\VM' `
-					-Generation 1 `
-					-Switch "NATSwitch"
-				Start-VM -Name FS01
-				New-VM -Name WEB01 `
-					-MemoryStartupBytes 2GB `
-					-BootDevice VHD `
-					-VHDPath 'C:\VM\WEB01.vhdx' `
-					-Path 'C:\VM' `
-					-Generation 1 `
-					-Switch "NATSwitch"
-				Start-VM -Name WEB01
-			}
-		}	
-  	}
+   node $nodeName
+   {
+       xVMSwitch InternalSwitch
+       {
+          Ensure	= 'Present'
+          Name		= 'NatSwitch'
+          Type		= 'Internal'
+       }
+
+       Script ConfigureHyperV
+       {
+          GetScript = 
+          {
+             @{Result = "ConfigureHyperV"}
+          }	
+          TestScript = 
+          {
+             return $false
+          }	
+          SetScript =
+          {
+             $zipDownload = "https://labnestedhvartifacts.blob.core.windows.net/azurelab/HyperVLabVMs.zip"
+             $downloadedFile = "D:\HyperVLabVMs.zip"
+             $vmFolder = "C:\VM"
+             Resize-Partition -DiskNumber 0 -PartitionNumber 2 -Size (400GB)
+             Invoke-WebRequest $zipDownload -OutFile $downloadedFile
+             Add-Type -assembly "system.io.compression.filesystem"
+             [io.compression.zipfile]::ExtractToDirectory($downloadedFile, $vmFolder)
+             $NatSwitch = Get-NetAdapter -Name "vEthernet (NatSwitch)"
+             New-NetIPAddress -IPAddress 192.168.0.1 -PrefixLength 24 -InterfaceAlias $NatSwitch.Name
+             New-NetNat -Name NestedVMNATnetwork -InternalIPInterfaceAddressPrefix 192.168.0.0/24 -Verbose
+             New-VM -Name DC01 `
+             	-MemoryStartupBytes 2GB `
+             	-BootDevice VHD `
+             	-VHDPath 'C:\VM\AD01.vhdx' `
+	        -Path 'C:\VM' `
+             	-Generation 1 `
+                -Switch "NATSwitch"
+             Start-VM -Name DC01
+             New-VM -Name FS01 `
+             	-MemoryStartupBytes 2GB `
+             	-BootDevice VHD `
+             	-VHDPath 'C:\VM\FS01.vhdx' `
+             	-Path 'C:\VM' `
+             	-Generation 1 `
+             	-Switch "NATSwitch"
+             Start-VM -Name FS01
+             New-VM -Name WEB01 `
+             	-MemoryStartupBytes 2GB `
+             	-BootDevice VHD `
+             	-VHDPath 'C:\VM\WEB01.vhdx' `
+             	-Path 'C:\VM' `
+             	-Generation 1 `
+             	-Switch "NATSwitch"
+             Start-VM -Name WEB01
+             Get-NetNat | Remove-NetNat -Confirm:$false
+         }
+      }	
+   }
 }
